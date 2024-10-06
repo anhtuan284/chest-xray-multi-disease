a16cbc731e6cf21066be1deb2ed111d80f431932
anhtuan284 <anhtuan28043@gmail.com>
2024-10-06 19:41:07 +0700

    fix: android cant load image from path

--- chest_xray_diagnostic/lib/screens/home.dart
+++ chest_xray_diagnostic/lib/screens/home.dart
@@ -21,36 +21,58 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Chest X-ray Diagnostic')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            DropdownButton<String>(
-              items: options.map((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-              onChanged: (String? newValue) {
-                setState(() {
-                  selectedOption = newValue!;
-                });
-              },
-              value: selectedOption,
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: isLoading
-                  ? null
-                  : () async {
-                      await showImagePickerDialog(context);
-                    },
-              child: isLoading
-                  ? const CircularProgressIndicator()
-                  : const Text('Chọn ảnh'),
-            )
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Instructions Text
+              const Text(
+                'Vui lòng chọn mô hình:',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              const Text(
+                'Đây chỉ là mô hình thử nghiệm, chọn mô hình để tiếp tục...',
+                style: TextStyle(fontSize: 14, color: Colors.grey),
+              ),
+              const SizedBox(height: 20),
+
+              // Dropdown for model selection
+              DropdownButton<String>(
+                items: options.map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    selectedOption = newValue!;
+                  });
+                },
+                value: selectedOption,
+              ),
+
+              const SizedBox(height: 20),
+
+              // Button to select an image
+              Center(
+                child: ElevatedButton(
+                  onPressed: isLoading
+                      ? null
+                      : () async {
+                          await showImagePickerDialog(context);
+                        },
+                  child: isLoading
+                      ? const CircularProgressIndicator()
+                      : const Text('Chọn ảnh'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -61,37 +83,51 @@ class _HomeScreenState extends State<HomeScreen> {
         await ImagePicker().pickImage(source: ImageSource.gallery);
 
     if (pickedFile != null) {
-      // Start loading and show a spinner
       setState(() {
         isLoading = true;
       });
 
-      // Fetch the images from the server
-      final List<XFile>? images = await fetchImagesFromServer(pickedFile);
+      // Check if YOLO or DenseNet is selected
+      if (selectedOption == 'DenseNet') {
+        // Fetch multiple images for DenseNet
+        final List<XFile>? images = await fetchImagesFromServer(
+            pickedFile, 'http://10.0.3.2:5000/densenet_predict');
+        setState(() {
+          isLoading = false;
+        });
 
-      // Stop loading once the images are fetched
-      setState(() {
-        isLoading = false;
-      });
+        if (images != null && images.isNotEmpty) {
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => DisplayImages(images: images),
+          ));
+        }
+      } else if (selectedOption == 'YOLO') {
+        // Fetch single image for YOLO
+        final XFile? image = await fetchSingleImageFromServer(
+            pickedFile, 'http://10.0.3.2:5000/yolo_predict');
+        setState(() {
+          isLoading = false;
+        });
 
-      // Navigate to the DisplayImages screen with the fetched images
-      if (images != null && images.isNotEmpty) {
-        Navigator.of(context).push(MaterialPageRoute(
-          builder: (context) => DisplayImages(images: images),
-        ));
+        if (image != null) {
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => DisplaySingleImage(image: image),
+          ));
+        }
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(
-            content: Text('No images found or error fetching data'),
+            content: Text('No model selected or error fetching data'),
           ),
         );
       }
     }
   }
 
-  Future<List<XFile>?> fetchImagesFromServer(XFile pickedFile) async {
+  Future<List<XFile>?> fetchImagesFromServer(
+      XFile pickedFile, String apiUrl) async {
     try {
-      final url = Uri.parse('http://127.0.0.1:5000/densenet_predict');
+      final url = Uri.parse(apiUrl);
 
       // Prepare the request and the image file bytes
       final request = http.MultipartRequest('POST', url);
@@ -107,34 +143,88 @@ class _HomeScreenState extends State<HomeScreen> {
       final response = await http.Response.fromStream(streamedResponse);
 
       if (response.statusCode == 200) {
-        // Decode the received ZIP file
-        final bytes = response.bodyBytes;
-        final archive = ZipDecoder().decodeBytes(bytes);
+        // If response is a ZIP file, extract images
+        final Uint8List zipBytes = response.bodyBytes;
 
-        // Extract each file and convert it into an XFile
-        List<XFile> extractedFiles = [];
-        for (var file in archive.files) {
-          if (file.isFile) {
-            final Uint8List fileBytes = file.content as Uint8List;
+        // Extract images from the zip file
+        final List<XFile> extractedImages =
+            await extractImagesFromZip(zipBytes);
 
-            // Create an XFile from the byte data
-            final xFile = XFile.fromData(
-              fileBytes,
-              name: file.name, // Set a name for the file
-              mimeType: 'image/jpeg', // Assuming the files are JPEG images
-            );
-
-            extractedFiles.add(xFile);
-          }
+        if (extractedImages.isNotEmpty) {
+          return extractedImages;
+        } else {
+          print('No images found in the response.');
+          return null;
         }
-
-        return extractedFiles;
       } else {
         print('Failed to load images. Status Code: ${response.statusCode}');
         return null;
       }
     } catch (e) {
       print('Error fetching images: $e');
+      return null;
+    }
+  }
+
+  Future<List<XFile>> extractImagesFromZip(Uint8List zipBytes) async {
+    // Decode the zip file
+    final Archive archive = ZipDecoder().decodeBytes(zipBytes);
+    final List<XFile> imageFiles = [];
+
+    // Iterate over each file in the zip
+    for (final ArchiveFile file in archive) {
+      if (file.isFile && file.name.endsWith('.jpg')) {
+        final Uint8List fileData = file.content as Uint8List;
+
+        // Convert the file data to XFile
+        final XFile xFile = XFile.fromData(
+          fileData,
+          name: file.name,
+          mimeType: 'image/jpeg',
+        );
+
+        imageFiles.add(xFile);
+      }
+    }
+
+    return imageFiles;
+  }
+
+  Future<XFile?> fetchSingleImageFromServer(
+      XFile pickedFile, String apiUrl) async {
+    try {
+      final url = Uri.parse(apiUrl);
+
+      // Prepare the request and the image file bytes
+      final request = http.MultipartRequest('POST', url);
+      final imageBytes = await pickedFile.readAsBytes();
+      request.files.add(http.MultipartFile.fromBytes(
+        'image',
+        imageBytes,
+        filename: 'chest_xray.jpg',
+      ));
+
+      // Send the request
+      final streamedResponse = await request.send();
+      final response = await http.Response.fromStream(streamedResponse);
+
+      if (response.statusCode == 200) {
+        final Uint8List bytes = response.bodyBytes;
+
+        // Create an XFile from the byte data
+        final xFile = XFile.fromData(
+          bytes,
+          name: 'yolo_output.jpg',
+          mimeType: 'image/jpeg',
+        );
+
+        return xFile;
+      } else {
+        print('Failed to load images. Status Code: ${response.statusCode}');
+        return null;
+      }
+    } catch (e) {
+      print('Error fetching image: $e');
       return null;
     }
   }
@@ -155,14 +245,107 @@ class DisplayImages extends StatelessWidget {
               itemCount: images.length,
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
+                crossAxisSpacing: 4.0,
+                mainAxisSpacing: 4.0,
               ),
               itemBuilder: (context, index) {
-                return Image.memory(
-                  File(images[index].path).readAsBytesSync(),
-                  fit: BoxFit.cover,
+                return GestureDetector(
+                  onTap: () {
+                    // Điều hướng sang trang xem ảnh full-size
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            FullImageScreen(imageFile: images[index]),
+                      ),
+                    );
+                  },
+                  child: FutureBuilder(
+                    future: images[index].readAsBytes(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(child: CircularProgressIndicator());
+                      } else if (snapshot.hasError) {
+                        return const Center(child: Text('Error loading image'));
+                      } else if (!snapshot.hasData || snapshot.data == null) {
+                        return const Center(child: Text('No image data found'));
+                      }
+
+                      return Image.memory(
+                        snapshot.data as Uint8List,
+                        fit: BoxFit.cover,
+                      );
+                    },
+                  ),
                 );
               },
             ),
+    );
+  }
+}
+
+class DisplaySingleImage extends StatelessWidget {
+  const DisplaySingleImage({super.key, required this.image});
+
+  final XFile image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('YOLO Prediction Result')),
+      body: Center(
+        child: FutureBuilder(
+          future: image.readAsBytes(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return const Text('Error loading image');
+            } else if (!snapshot.hasData || snapshot.data == null) {
+              return const Text('No image data found');
+            }
+
+            return Image.memory(
+              snapshot.data as Uint8List,
+              fit: BoxFit.contain,
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class FullImageScreen extends StatelessWidget {
+  const FullImageScreen({super.key, required this.imageFile});
+
+  final XFile imageFile;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Full Size Image')),
+      body: Center(
+        child: FutureBuilder(
+          future: imageFile.readAsBytes(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return const Text('Error loading image');
+            } else if (!snapshot.hasData || snapshot.data == null) {
+              return const Text('No image data found');
+            }
+
+            return InteractiveViewer(
+              child: Image.memory(
+                snapshot.data as Uint8List,
+                fit: BoxFit.contain,
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
